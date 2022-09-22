1b23c61b410250d371d90c4ea09a47f1ba0b11ff
0639Quercus <thsomsits.brie@gmail.com>
2022-09-22 14:24:01 +0700

    Complete lab-10

--- pages/api/room.js
+++ pages/api/room.js
@@ -2,4 +2,5 @@ import { readDB } from "../../backendLibs/dbLib";
 
 export default function roomRoute(req, res) {
   const rooms = readDB();
+  return res.json({ ok: true, rooms });
 }
--- pages/api/room/[roomId]/message/[messageId].js
+++ pages/api/room/[roomId]/message/[messageId].js
@@ -4,4 +4,20 @@ export default function roomIdMessageIdRoute(req, res) {
   //read value from URL
   const roomId = req.query.roomId;
   const messageId = req.query.messageId;
+
+  if (req.method !== "DELETE") return;
+
+  const rooms = readDB();
+  const roomIdx = rooms.findIndex((x) => x.roomId === roomId);
+  if (roomIdx === -1)
+    return res.status(404).json({ ok: false, message: "Invalid room id" });
+
+  const messageIdx = rooms[roomIdx].findIndex((x) => x.messageId === messageId);
+  if (messageIdx === -1)
+    return res.status(404).json({ ok: false, message: "Invalid message id" });
+
+  //valid request
+  rooms[roomIdx].splice(messageIdx, 1);
+  writeDB(rooms);
+  return res.json({ ok: true });
 }
