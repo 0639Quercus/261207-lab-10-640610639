36cfe0ce0e993b4ed3db42ae2937df2741889e9b
0639Quercus <thsomsits.brie@gmail.com>
2022-09-22 14:35:21 +0700

    Fix deletion bug and GET room respond

--- pages/api/room.js
+++ pages/api/room.js
@@ -1,6 +1,9 @@
 import { readDB } from "../../backendLibs/dbLib";
 
 export default function roomRoute(req, res) {
-  const rooms = readDB();
+  const roomsRead = readDB();
+  const rooms = roomsRead.map((x) => {
+    x.roomId, x.roomName;
+  });
   return res.json({ ok: true, rooms });
 }
--- pages/api/room/[roomId]/message/[messageId].js
+++ pages/api/room/[roomId]/message/[messageId].js
@@ -5,19 +5,21 @@ export default function roomIdMessageIdRoute(req, res) {
   const roomId = req.query.roomId;
   const messageId = req.query.messageId;
 
-  if (req.method !== "DELETE") return;
+  if (req.method === "DELETE") {
+    const rooms = readDB();
+    const roomIdx = rooms.findIndex((x) => x.roomId === roomId);
+    if (roomIdx === -1)
+      return res.status(404).json({ ok: false, message: "Invalid room id" });
 
-  const rooms = readDB();
-  const roomIdx = rooms.findIndex((x) => x.roomId === roomId);
-  if (roomIdx === -1)
-    return res.status(404).json({ ok: false, message: "Invalid room id" });
+    const messageIdx = rooms[roomIdx].messages.findIndex(
+      (x) => x.messageId === messageId
+    );
+    if (messageIdx === -1)
+      return res.status(404).json({ ok: false, message: "Invalid message id" });
 
-  const messageIdx = rooms[roomIdx].findIndex((x) => x.messageId === messageId);
-  if (messageIdx === -1)
-    return res.status(404).json({ ok: false, message: "Invalid message id" });
-
-  //valid request
-  rooms[roomIdx].splice(messageIdx, 1);
-  writeDB(rooms);
-  return res.json({ ok: true });
+    //valid request
+    rooms[roomIdx].messages.splice(messageIdx, 1);
+    writeDB(rooms);
+    return res.json({ ok: true });
+  }
 }
